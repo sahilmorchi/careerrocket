37de983b1b503592e0dd5bf1b38877236ff2aaf9
KarthikV <kvvetrivel@gmail.com>
2018-06-20 18:08:35 -0700

    adding sign in/up stuff.

--- js/components/loginpage.js
+++ js/components/loginpage.js
@@ -1,3 +1,9 @@
 // $(‘.message a’).click(function(){
 //     $(‘form’).animate({height: “toggle”, opacity: “toggle”}, “slow”);
 //   });
+
+// var registerForm = 
+
+$("#createAnAccount").click(function(event){
+    console.log("Hello!");
+});
