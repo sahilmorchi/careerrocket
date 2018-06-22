2537cc2d85260c9b89670b1f49cd2aec247e5c34
KarthikV <kvvetrivel@gmail.com>
2018-06-22 14:27:27 -0700

    -
    -

---
+++ public/js/components/studentmentor.js
@@ -0,0 +1,9 @@
+const submitStudent = () => {
+    console.log("Booper");
+    window.location = '../../studentdashboard/studentdashboard.html'; 
+}
+
+const submitMentor = () => {
+    console.log("Booperino");
+    window.location = '../../mentordashboard/mentordashboard.html'; 
+}
